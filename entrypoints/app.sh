af23fe3ccd493dcb7db2b73f1ef3c89de7ec38f0
David Rogers <rogersdpdr@gmail.com>
2022-05-09 22:26:21 +0200

    updated entrypoint

--- entrypoints/app.sh
+++ entrypoints/app.sh
@@ -7,6 +7,8 @@ export SECRET_KEY_BASE=$(rake secret)
 
 kill -9 $(cat /root/app/tmp/pids/server.pid)
 
+rm -f /root/app/tmp/pids/server.pid
+
 mkdir -p /root/app/tmp/downloads
 
 bundle exec rails server -b 0.0.0.0 -p 3000 -e docker
