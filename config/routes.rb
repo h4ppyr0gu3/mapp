e99c5e82334559e0c3c022c520e74a223b5a3e4a
David Rogers <rogersdpdr@gmail.com>
2022-06-09 09:14:30 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :songs
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root to: 'static_pages#home'
   get 'privacy', to: 'static_pages#privacy_policy'
   post 'contact_mail', to: 'static_pages#contact_mail'
