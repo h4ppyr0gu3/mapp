ab4bc2f201fd7dd78c4adb9b0aca93b7caea1875
David Rogers <rogersdpdr@gmail.com>
2022-06-10 21:38:58 +0200

    wip

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 uri = "redis://#{ENV['REDIS_URL']}:#{ENV['REDIS_PORT']}/0/mapp" || 'redis://localhost:6379/0/mapp'
-uri = "#{['REDIS_URL']}"
 
+uri = "redis://redis:6379"
 Rails.application.config.cache_store = :redis_store, uri
