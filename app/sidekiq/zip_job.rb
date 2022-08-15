2e65646afb36b2b683fddf6fa873fe5978fa39eb
David Rogers <rogersdpdr@gmail.com>
2022-08-15 19:50:48 +0200

    wip

--- app/sidekiq/zip_job.rb
+++ app/sidekiq/zip_job.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'securerandom'
 
 class ZipJob
   include Sidekiq::Job
@@ -9,16 +10,29 @@ class ZipJob
     params = JSON.parse(params).deep_symbolize_keys
     set_context(params)
     files = Downloads::UseCases::All.new(params:, context:).call
+    if files.empty?
+      notify_no_files
+      return
+    end
     filepaths = create_temp_dir(files)
-    create_zip_file(filepaths)
+    random_id = SecureRandom.hex(4)
+    create_zip_file(filepaths, random_id)
     Notification.create!(
       user_id: current_user.id,
-      text: "Your download is ready, <a href='#{ENV['API_URL']}/mapp_#{current_user.email}.zip'>here you go</a>",
+      text: "Your download is ready, <a href='#{ENV['API_URL']}/mapp_#{random_id}.zip'>here you go</a>",
       read: false
     )
   end
 
   private
+
+  def notify_no_files
+    Notification.create!(
+      user_id: current_user.id,
+      text: "All songs have been downloaded, wipe device?",
+      read: false
+    )
+  end
 
   def set_context(params)
     @params = params
@@ -31,32 +45,45 @@ class ZipJob
 
   def create_temp_dir(files)
     temp_folder = File.join(Dir.tmpdir, "mapp_#{current_user.email}")
+    FileUtils.rm_rf(temp_folder) if File.exist?(temp_folder)
     FileUtils.mkdir_p(temp_folder) unless Dir.exist?(temp_folder)
 
     files.map do |song|
-      filename = song.last.to_s
-      filepath = File.join temp_folder, filename
-      File.binwrite(filepath, song.first.download)
-      filepath
+      begin
+        filename = song.last.to_s
+        filepath = File.join temp_folder, filename
+        File.binwrite(filepath, song.first.download)
+        filepath
+      rescue ActiveStorage::FileNotFoundError
+        Rails.logger.info("Failed to find song: #{song}")
+      end
     end
   end
 
   # rubocop:disable Metrics/MethodLength
-  def create_zip_file(filepaths)
+  def create_zip_file(filepaths, random_id)
     require "zip"
     # security vulnerability being able to guess email addresses
-    zip_file = Rails.root.join("public", "mapp_#{current_user.email}.zip")
+    zip_file = Rails.root.join("public", "mapp_#{random_id}.zip")
     FileUtils.rm(zip_file) if File.exist?(zip_file)
     begin
       ::Zip::File.open(zip_file, create: true) do |zipfile|
         filepaths.each do |file|
-          filename = File.basename file
-          zipfile.add(filename, file)
+          next if file.is_a? Integer
+          begin
+            filename = File.basename file
+            zipfile.add(filename, file)
+          rescue ActiveStorage::FileNotFoundError
+            Rails.logger.info("Failed to find file: #{file}")
+          end
         end
       end
       zip_file
     ensure
-      filepaths.each { |filepath| FileUtils.rm(filepath) }
+      filepaths.each do |filepath| 
+        next if filepath.is_a? Integer
+        FileUtils.rm(filepath) 
+      end
     end
   end
   # rubocop:enable Metrics/MethodLength
