1aece2d299b5a1311abd4f79fd68f54c0ff2b06b
David Rogers <rogersdpdr@gmail.com>
2022-08-16 18:41:56 +0200

    wip

--- app/concepts/downloads/use_cases/update.rb
+++ app/concepts/downloads/use_cases/update.rb
@@ -4,15 +4,20 @@ module Downloads
   module UseCases
     class Update < ::UseCase::Base
       def call
+        if song.nil? || !song.mp3.attached? 
+          return
+        end
         # unless song.mp3.attached?
         # define in model
         # song.redownload
         # return nil unless song.mp3.attached?
         # end
 
-        current_device = current_user.devices.find_or_create_by(user_agent:)
-        repository.update_metadata(song) if song.updated != 2
-        DeviceSong.find_or_create_by(device_id: current_device.id, song_id: song.id)
+        ActiveRecord::Base.transaction do
+          current_device = current_user.devices.find_or_create_by(user_agent:)
+          repository.update_metadata(song) if song.updated != 2
+          DeviceSong.find_or_create_by(device_id: current_device.id, song_id: song.id)
+        end
         song
       end
 
