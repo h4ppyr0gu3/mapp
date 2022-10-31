b07e10eb4a3c81acf7b1864531bd0fd5743c1e03
david rogers <rogersdpdr@gmail.com>
2022-10-31 23:09:20 +0100

    comment out prometheus for a later date

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,16 +1,16 @@
 # in config/initializers/prometheus.rb
-if Rails.env != "test"
-  require 'prometheus_exporter/middleware'
+# if Rails.env != "test"
+#   require 'prometheus_exporter/middleware'
 
-  # This reports stats per request like HTTP status and timings
-  Rails.application.middleware.unshift PrometheusExporter::Middleware
-end
+#   # This reports stats per request like HTTP status and timings
+#   Rails.application.middleware.unshift PrometheusExporter::Middleware
+# end
 
-# in config/initializers/prometheus.rb
-if Rails.env != "test"
-  require 'prometheus_exporter/instrumentation'
+# # in config/initializers/prometheus.rb
+# if Rails.env != "test"
+#   require 'prometheus_exporter/instrumentation'
 
-  # this reports basic process stats like RSS and GC info, type master
-  # means it is instrumenting the master process
-  PrometheusExporter::Instrumentation::Process.start(type: "master")
-end
+#   # this reports basic process stats like RSS and GC info, type master
+#   # means it is instrumenting the master process
+#   PrometheusExporter::Instrumentation::Process.start(type: "master")
+# end
