900b5d3848243ecf62430288aa745cb48de4acef
david rogers <rogersdpdr@gmail.com>
2023-02-05 19:11:25 +0100

    wip

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,5 +69,7 @@ end
 # Default value for keep_releases is 5
 set :keep_releases, 3
 
+Rake::Task["deploy:assets:precompile"].clear_actions
+
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
