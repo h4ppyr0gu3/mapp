c6106a4a915600dd095d99abd04306f9e6d757a0
david rogers <rogersdpdr@gmail.com>
2023-02-05 21:23:22 +0100

    remove failing module

--- app/concepts/sidekiq/retry_monitoring_middleware/monitored_worker.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Sidekiq
-  module RetryMonitoringMiddleware
-    module MonitoredWorker
-    end
-  end
-end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,13 @@
 set :rails_env, 'production'
 
 server "ssh.h4ppyr0gu3.one", user: "david", roles: %w{app db web}
+
+namespace :deploy do
+  before :starting, :ensure_user do
+    #
+  end
+
+  after :finishing, :notify do
+    #
+  end
+end
