06441173618419857ea66c836c6cacfe7b8e6d96
david rogers <rogersdpdr@gmail.com>
2023-02-20 21:40:42 +0100

    add cap config and log emails to logster

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ server "ssh.h4ppyr0gu3.one", user: "david", roles: %w{app db web}
 namespace :deploy do
   after :"deploy:cleanup", :restart_service do
     on roles(:app) do 
-      ask(:sudo_password)
+      ask(:sudo_password, echo: false)
       execute("echo #{fetch(:sudo_password)} | sudo -S systemctl restart mapp")
       execute("echo #{fetch(:sudo_password)} | sudo -S systemctl restart mapp_sidekiq")
     end
