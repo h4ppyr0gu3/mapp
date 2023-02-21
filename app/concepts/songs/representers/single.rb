d7f7b37fc4792bc4daed7191a549637d4ac0b520
david rogers <rogersdpdr@gmail.com>
2023-02-21 08:33:50 +0100

    add base url to credentials

--- app/concepts/songs/representers/single.rb
+++ app/concepts/songs/representers/single.rb
@@ -16,7 +16,7 @@ module Songs
             genre: song.genre,
             album: song.album,
             year: song.year,
-            link: "#{ENV.fetch('BASE_URL')}/api/v1/download_redirect/#{song.id}",
+            link: "#{Rails.application.credentials['BASE_URL']}/api/v1/download_redirect/#{song.id}",
             seconds: song.seconds,
             image_url: song.image_url,
           }
